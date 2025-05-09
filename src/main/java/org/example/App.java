4c5e0c3a17084669f7273a5d8695da64e9767f89
Lotanna <tannaloamobi@gmail.com>
2025-05-09 17:19:54 +0100

    removed comments from main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,22 +3,7 @@ package org.example;
 public class App {
     public static void main(String[] args) {
 
-//        CardGame newCardGame = new CardGame();
-//
-//        System.out.println(newCardGame.getDeck());
-//        System.out.println(newCardGame.getDeck().size());
-//        System.out.println(newCardGame.shuffleDeck(newCardGame.getDeck()));
-//        System.out.println(newCardGame.dealCard());
-//        System.out.println(newCardGame.sortDeckInNumberOrder());
-//        System.out.println(newCardGame.sortDeckIntoSuits());
-
         Snap snapGame = new Snap();
-//        System.out.println(snapGame.getDeck());
-//        System.out.println();
-//        System.out.println(snapGame.getDeck().size());
-//        System.out.println();
         System.out.println(snapGame.startGame());
-//
-
     }
 }
